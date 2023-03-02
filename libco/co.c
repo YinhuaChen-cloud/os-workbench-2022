85997cdd7dbde15fc38ec50e6cb51b496d02cc81
tracer-nju <tracer@nju.edu.cn>
2023-03-03 06:57:16 +0800

    Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:57:16 up 14:47,  1 user,  load average: 0.04, 0.09, 0.09

--- libco/co.c
+++ libco/co.c
@@ -4,6 +4,10 @@
 struct co {
 };
 
+// co_start(name, func, arg) 创建一个新的协程，并返回一个指向 struct co 的指针 (类似于 pthread_create)。
+// * 新创建的协程从函数 func 开始执行，并传入参数 arg。新创建的协程不会立即执行，而是调用 co_start 的协程继续执行。
+// * 使用协程的应用程序不需要知道 struct co 的具体定义，因此请把这个定义留在 co.c 中；框架代码中并没有限定 struct co 结构体的设计，所以你可以自由发挥。
+// * co_start 返回的 struct co 指针需要分配内存。我们推荐使用 malloc() 分配。
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   return NULL;
 }
