91f91ccbc405a768504526c8e1b1838f44a97365
tracer-nju <tracer@nju.edu.cn>
2023-03-04 16:59:04 +0800

    Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:59:04 up 31 min,  1 user,  load average: 0.17, 0.12, 0.18

--- libco/co.c
+++ libco/co.c
@@ -30,7 +30,7 @@ int link_size = 0; // 链表大小
 #define LINK_MAXSIZE 128 // 链表最大长度为128
 
 // co_start(name, func, arg) 创建一个新的协程，并返回一个指向 struct co 的指针 (类似于 pthread_create)。
-// * 新创建的协程从函数 func 开始执行，并传入参数 arg。新创建的协程不会立即执行，而是调用 co_start 的协程继续执行。
+// * 新创建的协程从函数 func 开始执行，并传入参数 arg。新创建的协程不会立即执行，而是(调用 co_start 的协程)(这个“调用 co_start 的协程就是main啦”)继续执行。
 // * 使用协程的应用程序不需要知道 struct co 的具体定义，因此请把这个定义留在 co.c 中；框架代码中并没有限定 struct co 结构体的设计，所以你可以自由发挥。
 // * co_start 返回的 struct co 指针需要分配内存。我们推荐使用 malloc() 分配。
 // name: 协程名字
