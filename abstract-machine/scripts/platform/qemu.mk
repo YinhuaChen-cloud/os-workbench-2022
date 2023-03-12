55a55fc1f185a322353e9296b7263a235c384d75
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-03-13 01:09:56 +0800

    2023-3-13, daily update on comments

--- abstract-machine/scripts/platform/qemu.mk
+++ abstract-machine/scripts/platform/qemu.mk
@@ -7,11 +7,17 @@ QEMU_FLAGS += -serial mon:stdio \
               -drive format=raw,file=$(IMAGE)
 
 build-arg: image
+	# mainargs 一般是 空
+	@echo "In build-arg, mainargs = $(mainargs)"
 	@( echo -n $(mainargs); ) | dd if=/dev/stdin of=$(IMAGE) bs=512 count=2 seek=1 conv=notrunc status=none
 
 BOOT_HOME := $(AM_HOME)/am/src/x86/qemu/boot
 
-image: $(IMAGE).elf
+# 同名的目标：它们共享prerequisite，所有prerequisite都满足才能编译
+# 但是，下面跟着命令的目标只能有一个，参考这个网页 https://stackoverflow.com/questions/43718595/two-targets-with-the-same-name-in-a-makefile
+image: $(IMAGE).elf 
+	# IMAGE = amgame/build/amgame-x86_64-qemu
 	@$(MAKE) -s -C $(BOOT_HOME)
+	# IMAGE_REL = build/amgame-x86_64-qemu
 	@echo + CREATE "->" $(IMAGE_REL)
-	@( cat $(BOOT_HOME)/bootblock.o; head -c 1024 /dev/zero; cat $(IMAGE).elf ) > $(IMAGE)
+	( cat $(BOOT_HOME)/bootblock.o; head -c 1024 /dev/zero; cat $(IMAGE).elf ) > $(IMAGE)
